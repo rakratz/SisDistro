13547c4e59d5d5310031f92f0da977f27e79158e
Prof. Me. Ricardo de Andrade Kratz <rakratz@gmail.com>
2026-03-16 08:12:13 -0300

    Refactor User class to include departament methods
    Added getter and setter for 'departament' field.

--- SisDistro07/src/main/java/com/example/SisDistro07/model/User.java
+++ SisDistro07/src/main/java/com/example/SisDistro07/model/User.java
@@ -1,60 +1,61 @@
 package com.example.SisDistro07.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "tb_user")
 public class User {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private String name;
-	private String email;
-	
-	
-	@ManyToOne
-	@JoinColumn(name = "department_id")
-	private Departament departament;
-	
-	public User() {
 
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	public User(Long id, String name, String email, Departament departament) {
-		this.id = id;
-		this.name = name;
-		this.email = email;
-		this.departament= departament;
-	}
+    private String name;
+    private String email;
 
-	public Long getId() {
-		return id;
-	}
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Departament departament;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public User() {
+    }
 
-	public String getName() {
-		return name;
-	}
+    public User(Long id, String name, String email, Departament departament) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.departament = departament;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Departament getDepartament() {
+        return departament;
+    }
+
+    public void setDepartament(Departament departament) {
+        this.departament = departament;
+    }
 }
